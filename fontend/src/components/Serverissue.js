feb4b7d7961a4bdc6be6c41b98539a3df1794b6e
Vaibhavsurvase1997 <vaibhav.survase@neosoftmail.com>
2022-01-27 12:55:23 +0530

    first commit

---
+++ fontend/src/components/Serverissue.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Serverissue extends Component {
+    render() {
+        return (
+            <div>
+                <img src="./ab.webp" height={"800px"} width={"1500px"}></img>
+            </div>
+        )
+    }
+}
+
+export default Serverissue
